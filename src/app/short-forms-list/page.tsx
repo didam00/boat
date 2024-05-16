fa1ad7b893b8979116b6fb52aec1f45bdca187df
didam00 <didam00@naver.com>
2024-05-16 14:44:11 +0900

    :bug: add unique keys to form rows and short forms

--- src/app/short-forms-list/page.tsx
+++ src/app/short-forms-list/page.tsx
@@ -7,6 +7,7 @@ export default function ShortFormsList() {
   for (let i = 1; i <= 16; i++) {
     shortForms.push(
       <ShortForm
+        key={`short_form-${i}`}
         title={`타이틀 제목 ${i}`}
         desc="쇼트 폼(설문)에 대한 설명을 상세히 적습니다. 이미지를 포함해도 됩니다."
       />
