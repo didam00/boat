e5002f7ba82e18cb05288b29f14a87c7f29f2937
didam00 <didam00@naver.com>
2024-06-05 22:10:02 +0900

    🐛 fix multiple connection to mongodb error

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -2,7 +2,13 @@ import mongoose, { mongo } from 'mongoose'
 
 const DB_URI = process.env.MONGO_URI;
 
+let isConnected = false;
+
 export default function connect() {
+  if (isConnected) {
+    return;
+  }
+
   try {
     mongoose.connect(DB_URI!);
     const connection = mongoose.connection;
@@ -15,7 +21,11 @@ export default function connect() {
       console.log("MongoDB connection error" + err);
       process.exit();
     })
+
+    isConnected = true;
+
   } catch (error) {
-    console.log(error);
+    console.log('MongoDB connection error: ', error);
+    process.exit();
   }
 }
