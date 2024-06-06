ab76d9914a87a6d7f3dd1ea5a1e6cd02bd0a650b
didam00 <didam00@naver.com>
2024-06-06 20:26:46 +0900

    ✨ add middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const onlyVerified = ["/form/create"];
+  const onlyNonVerified = ["/login", "/register"];
+  const token = req.cookies.get('token')?.value || "";
+
+  if (onlyNonVerified.includes(path) && token) {
+    return NextResponse.redirect(
+      new URL('/', req.nextUrl)
+    );
+  }
+  
+  if (onlyVerified.includes(path) && !token) {
+    return NextResponse.redirect(
+      new URL('/login', req.nextUrl)
+    );
+  }
+};
+
+export const config = {
+  matcher: [
+    "/form/create",
+    "/login",
+    "/register"
+  ]
+};
