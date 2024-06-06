507e10006fa8b8adcade4146f81dbb7edda0d45f
didam00 <didam00@naver.com>
2024-06-06 11:46:21 +0900

    🐛 logout bug (route)

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function LogoutPage() {
+  const router = useRouter();
+
   const logout = async () => {
     try {
       await axios.get('/api/users/logout');
+      router.push("/");
     } catch (error: any) {
       console.log(error.message)      
     }
