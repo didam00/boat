0c8d7673090c8b95e981e3d8ed8f18418b0c212a
didam00 <didam00@naver.com>
2024-06-10 17:50:36 +0900

    💄 add my page ui (2)

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -28,11 +28,14 @@ export default function ProfilePage() {
       <div className="m__size">
         <div className={`box-container ${styles["profile-box"]}`}>
           <h2>{user.nickname}</h2>
+          <RowComponent property="이메일" value={user.email} />
           <RowComponent property="유저 아이디" value={user.username} />
-          <RowComponent property="유저 아이디" value={user.username} />
-          <RowComponent property="유저 아이디" value={user.username} />
-          <RowComponent property="유저 아이디" value={user.username} />
-          <RowComponent property="유저 아이디" value={user.username} />
+          <RowComponent property="이름" value={user.name} />
+          <RowComponent property="전화번호" value={user.phoneNumber} />
+          <RowComponent property="지역" value={user.address.country + " " + user.address.city} />
+          <RowComponent property="생일" value={`${user.birth.getFullYear()}년 ${user.birth.getMonth()+1}월 ${user.birth.getDate()}일`} />
+          <RowComponent property="직업" value={user.job} />
+          <RowComponent property="성별" value={user.gender} />
         </div>
       </div>
     </main>
